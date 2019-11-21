afca96fba11ef2c9978c393051125ce75d4af191
Steven Okpysh <46796395+bigstevo@users.noreply.github.com>
2019-11-21 11:36:40 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using YummyMummy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace YummyMummy
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<RecipeDBcontext>(options => options.UseSqlServer(Configuration["Data:YummyMummy:ConnectionString"]));
+			services.AddTransient<IRecipeRepository,EFRecipeRepository>();
+			//services.AddTransient<IRecipeRepository,FakeRecipeRepository>();
+			services.AddMvc();
+			
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseDeveloperExceptionPage();
+			app.UseStatusCodePages();
+
+			if (env.IsDevelopment())
+			{
+				app.UseBrowserLink();
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+
+			app.UseStaticFiles();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+			SeedData.EnsurePopulated(app);
+		}
+
+
+		
+	}
+}
